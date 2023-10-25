7f3b0835d60464f63c745af32b4b4f0ea2e7622c
Dariusz Wilk <dariusz.wilk.1993@gmail.com>
2023-10-25 13:03:47 +0100

    Rendered view of single posts

--- src/components/pages/SinglePost/SinglePost.js
+++ src/components/pages/SinglePost/SinglePost.js
@@ -1,15 +1,37 @@
 import { useSelector } from 'react-redux';
-import { Navigate, useParams } from 'react-router-dom';
+import { Link, Navigate, useParams } from 'react-router-dom';
 import { getPostsById } from '../../../redux/store';
+import { Button } from 'react-bootstrap';
+import EditPost from '../EditPost/EditPost';
 
 const SinglePost = () => {
 	const { id } = useParams();
 	const postData = useSelector(state => getPostsById(state, id));
+	console.log(postData);
 
 	if (!postData) return <Navigate to={'/'} />;
 	return (
 		<div>
-			<h1>SinglePost... {id}</h1>
+			<div className="d-flex justify-content-between align-items-center mb-4">
+				<h1>{postData.title}</h1>
+				<div className="buttons">
+					<Link to={`/post/edit/${id}`} element={<EditPost />}>
+						<Button variant="outline-info">Edit</Button>
+					</Link>
+					<Button variant="outline-danger" className="ms-3">
+						Delete
+					</Button>
+				</div>
+			</div>
+			<p className="mb-1">
+				<b>Author: </b>
+				{postData.author}
+			</p>
+			<p>
+				<b>Published: </b>
+				{postData.publishedDate}
+			</p>
+			<article>{postData.content}</article>
 		</div>
 	);
 };
