fadc204f554dc23d48c89df9d60eca152f341fd2
Dariusz Wilk <dariusz.wilk.1993@gmail.com>
2023-10-26 21:31:28 +0100

    cleared code from console.log

--- src/components/features/Posts/Posts.js
+++ src/components/features/Posts/Posts.js
@@ -4,7 +4,6 @@ import Post from '../Post/Post';
 
 const Posts = () => {
 	const posts = useSelector(state => getAllPosts(state));
-	console.log(posts);
 
 	return (
 		<ul className="d-flex flex-wrap justify-content-start p-0 my-4">
