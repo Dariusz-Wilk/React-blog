54796ca61cd76aa2a6f4063d4dea51ac1823d03a
Dariusz Wilk <dariusz.wilk.1993@gmail.com>
2023-10-26 21:01:22 +0100

    AddPost feature implemented

--- src/components/features/AddPostForm/AddPostForm.js
+++ src/components/features/AddPostForm/AddPostForm.js
@@ -1,25 +1,81 @@
 import { Button, Form } from 'react-bootstrap';
 import styles from './AddPostForm.module.scss';
 
+import { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { addPost } from '../../../redux/postsRedux';
+import { useNavigate } from 'react-router-dom';
+
 const AddPostForm = () => {
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
+	const [title, setTitle] = useState('');
+	const [author, setAuthor] = useState('');
+	const [publishedDate, setPublishedDate] = useState('');
+	const [description, setDescription] = useState('');
+	const [mainContent, setMainContent] = useState('');
+
+	const postArrLength = useSelector(state => state.posts.length);
+	const newPostId = postArrLength + 1 + '';
+
+	const addNewPost = e => {
+		e.preventDefault();
+
+		dispatch(
+			addPost({
+				id: newPostId,
+				title,
+				shortDescription: description,
+				content: mainContent,
+				publishedDate,
+				author,
+			})
+		);
+
+		setTitle('');
+		setAuthor('');
+		setPublishedDate('');
+		setDescription('');
+		setMainContent('');
+
+		navigate('/');
+	};
+
 	return (
-		<Form className="mt-4">
+		<Form className="mt-4" onSubmit={addNewPost}>
 			<Form.Group className="mb-3">
 				<Form.Label>Title:</Form.Label>
-				<Form.Control type="text" placeholder="Enter title..." />
+				<Form.Control
+					type="text"
+					placeholder="Enter title..."
+					value={title}
+					onChange={e => setTitle(e.target.value)}
+				/>
 			</Form.Group>
 			<Form.Group className="mb-3">
 				<Form.Label>Author:</Form.Label>
-				<Form.Control type="text" placeholder="Enter author..." />
+				<Form.Control
+					type="text"
+					placeholder="Enter author..."
+					value={author}
+					onChange={e => setAuthor(e.target.value)}
+				/>
 			</Form.Group>
 			<Form.Group className="mb-3">
 				<Form.Label>Published:</Form.Label>
-				<Form.Control type="date" placeholder="Enter published date..." />
+				<Form.Control
+					type="date"
+					placeholder="Enter published date..."
+					value={publishedDate}
+					onChange={e => setPublishedDate(e.target.value)}
+				/>
 			</Form.Group>
 			<Form.Group className="mb-3">
 				<Form.Label>Short description:</Form.Label>
 				<Form.Control
 					className={styles.textareaDescription}
+					onChange={e => setDescription(e.target.value)}
+					value={description}
 					as="textarea"
 					placeholder="Enter post description..."
 				/>
@@ -29,6 +85,8 @@ const AddPostForm = () => {
 				<Form.Label>Main content:</Form.Label>
 				<Form.Control
 					className={styles.textareaMain}
+					value={mainContent}
+					onChange={e => setMainContent(e.target.value)}
 					as="textarea"
 					placeholder="Enter post content..."
 				/>
--- src/redux/postsRedux.js
+++ src/redux/postsRedux.js
@@ -3,8 +3,11 @@
 //actions
 const createActionName = actionName => `app/posts/${actionName}`;
 const REMOVE_POST = createActionName('REMOVE_POST');
+const ADD_POST = createActionName('ADD_POST');
 
 export const removePost = payload => ({ type: REMOVE_POST, payload });
+
+export const addPost = payload => ({ type: ADD_POST, payload });
 
 //action creators
 
@@ -12,7 +15,8 @@ const postsReducer = (statePart = [], action) => {
 	switch (action.type) {
 		case REMOVE_POST:
 			return statePart.filter(post => post.id !== action.payload);
-
+		case ADD_POST:
+			return [...statePart, { ...action.payload }];
 		default:
 			return statePart;
 	}
